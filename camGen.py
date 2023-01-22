2bb5a8c68ba8bcad76dd3e729c6573283d5f4251
homer2020 <ericmbaird@gmail.com>
2023-01-22 03:03:48 -0500

    load animations from list

--- camGen.py
+++ camGen.py
@@ -36,17 +36,26 @@ def get_animation_type(animations):
                 print("Invalid input, please enter a valid number.")
 
 
+import re
+
 def get_seconds():
+    # Loop until user enters valid input
     while True:
+        # Ask user for seconds input
         seconds_string = input("Enter the seconds separated by commas: ")
+        # Check if input is empty
         if not seconds_string.strip():
             print("You entered nothing, please enter a valid number.")
             continue
+        # Extract seconds from input
         seconds = re.findall(r"[-+]?\d*\.\d+|\d+", seconds_string)
+        # Check if seconds were extracted
         if not seconds:
             print("Invalid input. Please enter a valid number.")
             continue
+        # Convert seconds to float and return
         return [float(x) for x in seconds]
+
 
 
 def create_another_file():
@@ -70,37 +79,27 @@ def create_file_loop(file_name):
 
 
 def add_animations_loop(file_name):
-    # Loop until user chooses to exit
+    # Load animation types from file
+    with open('animation_types.txt', 'r') as f:
+        animations = f.readlines()
+    animations = [x.strip() for x in animations]
     while True:
-        # Get animation type from user
-        animation_type = get_animation_type(animations)
-        # If animation type is None, continue to next iteration
-        if animation_type is None:
-            continue
-        # Get seconds from user
-        seconds = get_seconds()
-        # If seconds is None, continue to next iteration
-        if seconds is None:
-            continue
-        # Remove negative seconds
-        seconds = [x for x in seconds if x>=0]
-        # Check if seconds list is not empty
-        if not seconds:
-            print("Invalid input. Please enter a valid number.")
-            continue
-        # Create file with animation and seconds
-        create_file(file_name, seconds, animation_type)
-        # Loop until user enters valid input
-        while True:
-            # Ask user if they want to add another animation
-            another_animation = input("Do you want to add another animation to the file? (y/n) ").lower()
-            # Check if input is valid
-            if another_animation in ["y", "n"]:
-                break
-            print("Invalid input. Please enter 'y' or 'n'.")
-        # If user doesn't want to add another animation, exit loop
-        if another_animation != "y":
-            return
+        # Print animation options
+        print("Choose an animation type to add to the file:")
+        for index, animation_type in enumerate(animations):
+            print(f"{index+1}: {animation_type}")
+        print("0: Exit")
+        # Ask user to enter animation type choice
+        choice = int(input())
+        # Check if choice is 0, if so return None
+        if choice == 0:
+            return None
+        # Check if choice is a valid animation type
+        elif choice in range(1, len(animations)+1):
+            animation_type = animations[choice-1]
+        else:
+            print("Invalid choice, please enter a valid number.")
+
 
 def create_animation_file(file_name):
     # Create file and handle errors
@@ -118,18 +117,7 @@ def create_animation_file(file_name):
     # Return True if user wants to create another file, False otherwise
     return create_another == "y"
 
-animations = {
-    1: "translation_x",
-    2: "translation_y",
-    3: "translation_z",
-    4: "rotation_3d_x",
-    5: "rotation_3d_y",
-    6: "rotation_3d_z",
-    7: "noise_schedule",
-    8: "strength_schedule",
-    9: "contrast_schedule",
-    10: "cfg_scale_schedule"
-}
+
 while create_animation_file(input("Enter the file name: ") + "Camera"):
     pass
 
